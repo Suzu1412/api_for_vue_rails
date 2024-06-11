9352e2857a18d1179da7060bb201f8e703a583d6
Suzu <minato1932@gmail.com>
2024-06-11 11:07:24 -0400

    Implementing First Form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 import Posts from './components/Posts.vue'
 
 </script>
@@ -8,34 +7,13 @@ import Posts from './components/Posts.vue'
 <template>
   <main>
     <Posts />
-  </main>
+</main>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  html {
+    background-color: #2f3640;
+    padding: 2rem;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,11 +1,165 @@
 <template>
-    <div>
-        <h1>Posts </h1>
-    </div>
-</template>
+<v-sheet class="pa-12" rounded>
+    <v-card class="mx-auto px-6 py-8" max-width="344">
+      <v-form ref ="form"
+        validate-on="input"
+        @submit.prevent="onSubmit"
+      >
+        <v-text-field
+          v-model="title"
+          :readonly="loading"
+          :rules="[required]"
+          class="mb-2"
+          label="Título"
+          clearable
+        ></v-text-field>
+
+        <v-text-field
+          v-model="body"
+          :readonly="loading"
+          :rules="[required]"
+          label="Texto"
+          placeholder="Ingresa el texto"
+          clearable
+        ></v-text-field>
+
+        <br>
+
+        <v-btn v-if="isEditing"
+          @click="updatePost"
+          :disabled="!form"
+          :loading="loading"
+          color="success"
+          size="large"
+          type="submit"
+          variant="elevated"
+          block
+        >
+          Actualizar Post
+        </v-btn>
+
+        <v-btn v-if="isEditing"
+          @click="cancelEdit"
+          :disabled="!form"
+          :loading="loading"
+          color="error"
+          size="large"
+          type="submit"
+          variant="elevated"
+          block
+        >
+          Cancelar
+        </v-btn>
+
+        <v-btn v-else
+          @click="createPost"
+          :disabled="!valid"
+          :loading="loading"
+          color="primary"
+          size="large"
+          type="submit"
+          variant="elevated"
+          block
+        >
+          Crear nuevo Post
+        </v-btn>
+
+        <v-btn
+          @click="reset"
+          color="info"
+          size="large"
+          type="submit"
+          variant="elevated"
+          block
+        >
+          Reset
+        </v-btn>
+
+      </v-form>
+    </v-card>
+  </v-sheet>
+  </template>
 
 
-<script setup>
+<script>
+    import { ref, onMounted } from 'vue'
+
+    const posts = ref([])
+    const API_URL = "http://localhost:3000/posts"   
+
+    const updatePost = async() => {
+        return true;
+    }
+
+    const cancelEdit = () => {
+        return true;
+    }
+
+    const createPost = async() => {
+        const res = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: title.value,
+                body: body.value
+            })
+        } )
+
+        const dataResponse = await res.json()
+
+        posts.value.push(data);
+        title: null
+    }
+
+    export default {
+    data: () => ({
+      form: false,
+      title: null,
+      body: null,
+      valid: false,
+      loading: false,
+      isEditing: false,
+    }),
+
+    methods: {
+      async createPost () {
+        const { valid } = await this.$refs.form.validate()
+        if (!valid) return
+
+        console.log("Creando post");
+      },
+      async updatePost () {
+        console.log("Actualizando post");
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      onInput(){
+        console.log("escribiendo");
+        valid = this.$refs.form.validate()
+      },
+
+
+      onSubmit () {
+
+        console.log("escribiendo");
+        valid = this.$refs.form.validate()
+        //const { valid } = this.$refs.form.validate()
+
+        //if (!valid) return
+
+        //this.loading = true
+
+        //setTimeout(() => (this.loading = false), 2000)
+      },
+      required (input) {
+        return !!input || 'Campo requerido'
+      },
+    }
+    
+  }
 </script>
 
 <style scoped>
