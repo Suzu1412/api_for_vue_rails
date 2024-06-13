3bc4e3ddc9aab2632507dc27b64edff2f1af3cc2
Suzu <minato1932@gmail.com>
2024-06-13 04:29:45 -0400

    Agregadas notas para un futuro

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,      
         ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''), // Requerido para solucionar CORS
       }
     },
   },
